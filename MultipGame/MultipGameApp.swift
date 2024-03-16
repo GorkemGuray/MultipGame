b001ee32dc39ee4535b846b10cea6c9019aff991
Görkem Güray <grkmgry@gmail.com>
2024-03-16 11:43:08 +0300

    Initial Commit

---
+++ MultipGame/MultipGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipGameApp.swift
+//  MultipGame
+//
+//  Created by Görkem Güray on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultipGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
